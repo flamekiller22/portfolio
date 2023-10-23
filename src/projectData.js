8996fff4a3e35f6375669a1069154ee042d87abd
flamekiller22 <flamekillerace@gmail.com>
2023-10-23 15:03:33 +0530

    update

--- src/projectData.js
+++ src/projectData.js
@@ -4,24 +4,21 @@ const projects = [
     image: './images/Project-1.png',
     description: "This project was made to prevent proxy system and get attendance of students through their fingerprints and face recognition using different Machine Learning techniques. My role was to secure the database and mantain the back-end for the Web Application.",
     tools: ['ReactJs', 'Tailwind css'],
-    github: 'https://github.com/codewithvk/React-simple-portfolio',
-    // link: 'https://github.com/codewithvk/React-simple-portfolio',
+    github: 'https://github.com/flamekiller22/cyberpe',
   },
   {
     title: 'NFC Based Payment System',
     image: './images/Project-2.jpg',
     description: "It was team project made in order to ease the payment system in our institution premises as a subsitute of UPI payment system.Here we used NFC enabled student identity cards in order to pay for various items at different shops in the campus.",
     tools: ['ReactJs', 'Tailwind css'],
-    github: 'https://github.com/codewithvk/React-simple-portfolio',
-    // link: 'https://github.com/codewithvk/React-simple-portfolio',
+    github: 'https://github.com/flamekiller22/Biometric-Attendance-Management-System',
   },
   {
     title: 'My Portfolio Site',
     image: './images/portfolio.png',
     description: "Make and host my portfolio!!",
     tools: ['ReactJs', 'Tailwind css'],
-    github: 'https://github.com/codewithvk/React-simple-portfolio',
-    // link: 'https://github.com/codewithvk/React-simple-portfolio',
+    github: 'https://github.com/flamekiller22/portfolio',
   },
   
 ];
